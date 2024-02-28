de6c4a98434a1d524928849df0e0ada7711c0d4c
Pardallzim <rogerio.silva220304@gmail.com>
2024-02-28 19:13:08 -0300

    Atualização dos exercicios

--- ListaIF-Rogerio/ex-14.py
+++ ListaIF-Rogerio/ex-14.py
@@ -3,3 +3,18 @@ l = int(input("Digite a largura (em cm): "))
 c = int(input("Digite a comprimento (em cm): "))
 
 cd = int(input("Digite o consumo de água (em L/dias): "))
+
+x = (a * l * c) / 1000
+y = x // cd
+
+print("capacidade em m³ é de: ", (a * l * c)//1000000)
+
+print("A capacidade total do reservatorio é de: ", x,"Litros")
+print("A autonomia do reservatorio é de: ", y,"Dias")
+
+if y < 2:
+    print("Consumo elevado")
+elif y >= 2 and y < 7:
+    print("Consumo moderado")
+else:
+    print("Consumo reduzido")
