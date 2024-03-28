f714a59c5d667ae3152d79c3846fbd00577cc64d
Pardallzim <rogerio.silva220304@gmail.com>
2024-03-27 22:01:53 -0300

    Aviao

---
+++ Rogerio_aviao/ex-01.py
@@ -0,0 +1,10 @@
+#1) A classe Avião poderia ter uma associação com a classe Coordenada Geográfica, pois
+#esta ultima consegue representar a latitude, longitude e permite ainda indicar a
+#distância em km entre duas Coordenada Geográfica
+#  A associação entre essas classes seria do tipo Agregação ou Composição?
+#  Quais atributos e métodos deverão permanecer na classe Avião? 
+#2) Crie um programa para representar funcionários em um sistema de gestão de
+#pessoas. 
+#É necessário que nesta modelagem seja feito de uso de no mínimo duas classes e
+#com uma associação entre estas.
+
